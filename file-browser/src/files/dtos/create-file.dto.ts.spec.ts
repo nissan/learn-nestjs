6013c148edc6424f0a664c7261132794114728a3
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-26 08:02:40 +1000

    scaffold module, controller, service, entities, dtos

---
+++ file-browser/src/files/dtos/create-file.dto.ts.spec.ts
@@ -0,0 +1,7 @@
+import { CreateFileDto.Ts } from './create-file.dto.ts';
+
+describe('CreateFileDto.Ts', () => {
+  it('should be defined', () => {
+    expect(new CreateFileDto.Ts()).toBeDefined();
+  });
+});
---
+++ file-browser/src/files/dtos/update-file.dto.ts.spec.ts
@@ -0,0 +1,7 @@
+import { UpdateFileDto.Ts } from './update-file.dto.ts';
+
+describe('UpdateFileDto.Ts', () => {
+  it('should be defined', () => {
+    expect(new UpdateFileDto.Ts()).toBeDefined();
+  });
+});
