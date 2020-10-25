e9ded01fbe1789cfe6c8e8443dddc96180733f5a
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-25 22:15:28 +1000

    replace controller functions with service implementations

--- hello-world/src/coffees/coffees.controller.ts
+++ hello-world/src/coffees/coffees.controller.ts
@@ -8,44 +8,28 @@ export class CoffeesController {
     }
     @Get()
     findAll(@Query() paginationQuery){
-        const {limit, offset} = paginationQuery;
-        return `Found all coffees. Limit: ${limit}, offset: ${offset}`;
-    }
-
-    @Get('flavours')
-    findFlavours(@Res() response){
-        // this approach to use expressjs standard
-        // methods limits compatibility with nest standard decorators
-        // like @HttpCode as well as nest standard interceptors
-        // and makes this platform dependent
-        response.status(HttpStatus.OK).send("Found all flavours of coffee");
+        // const {limit, offset} = paginationQuery;
+        return this.coffeeService.findAll();
     }
 
     @Get(':id')
     findOne(@Param('id') id:string) {
-        return `This returns ${id} coffee`;
+        return this.coffeeService.findOne(id);;
     }
 
     @Post('name')
     @HttpCode(HttpStatus.GONE)
     createName(@Body('name') body){
-        return body;
-        //return "This creates a new coffee";
-    }
-
-    @Post()
-    create(@Body() body){
-        return body;
-        //return "This creates a new coffee";
+       return this.coffeeService.create(body);
     }
 
     @Patch(':id')
     update(@Param('id') id:string, @Body() body){
-        return `This action updates #${id} coffee`;
+        return this.coffeeService.update(id, body);
     }
 
     @Delete(':id')
     remove(@Param('id') id:string){
-        return `This action deletes #${id} coffee`;
+        return this.coffeeService.remove(id);
     }
 }
