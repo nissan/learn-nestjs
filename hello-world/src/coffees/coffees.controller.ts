bb5e3f472926562e45e01c67fa1ac8f623ed2ace
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-25 20:32:10 +1000

    add sample patch and delete methods

--- hello-world/src/coffees/coffees.controller.ts
+++ hello-world/src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -27,5 +27,15 @@ export class CoffeesController {
     create(@Body() body){
         return body;
         //return "This creates a new coffee";
+    }
+
+    @Patch(':id')
+    update(@Param('id') id:string, @Body() body){
+        return `This action updates #${id} coffee`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id:string){
+        return `This action deletes #${id} coffee`;
     }
 }
